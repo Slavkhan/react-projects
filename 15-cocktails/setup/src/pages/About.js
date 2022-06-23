5930a1b778a1def8d09a22ba15e1a04e2f11bafd
Slavisa <petrovic.s1012@hotmail.com>
2022-06-23 09:06:58 +0200

    updateJune23

--- 15-cocktails/setup/src/pages/About.js
+++ 15-cocktails/setup/src/pages/About.js
@@ -1,11 +1,18 @@
-import React from 'react'
+import React from "react";
 
 const About = () => {
   return (
-    <div>
-      <h2>about page</h2>
-    </div>
-  )
-}
+    <section className="section about-section">
+      <h1 className="section-title">about page</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima officiis
+        minus, sunt nostrum voluptate itaque unde aut alias est accusamus sint.
+        Corrupti provident consequatur cupiditate ratione natus ipsum iusto
+        aliquid modi est! Quaerat, unde? Quo maxime odit explicabo quibusdam
+        ipsa!
+      </p>
+    </section>
+  );
+};
 
-export default About
+export default About;
--- 15-cocktails/setup/src/pages/Error.js
+++ 15-cocktails/setup/src/pages/Error.js
@@ -1,12 +1,17 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Error = () => {
   return (
-    <div>
-      <h2>error page</h2>
-    </div>
-  )
-}
+    <section className="error-page section">
+      <div className="error-container">
+        <h2>oops! it's a dead end</h2>
+        <Link to="/" className="btn">
+          back home
+        </Link>
+      </div>
+    </section>
+  );
+};
 
-export default Error
+export default Error;
